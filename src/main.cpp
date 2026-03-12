989dffd7540e689d960a56587a411d7b3ac67ebd
sryerabati <shreyas.yerabati06@gmail.com>
2026-03-12 02:47:53 -0400

    up

--- src/main.cpp
+++ src/main.cpp
@@ -56,30 +56,30 @@ void waitForStartLight()
     LCD.Clear();
 }
 
-void moveBackward(float distanceInches)
+void moveBackward(float distanceInches, int powerPercent = 40)
 {
     int direction = 1;
     right_encoder.ResetCounts();
     left_encoder.ResetCounts();
 
     int distanceCounts = (distanceInches * 318)/7.85; // Convert inches to encoder counts
-    rightMotor.SetPercent(direction * 40);
-    leftMotor.SetPercent(-1* direction * 40);
+    rightMotor.SetPercent(direction * powerPercent);
+    leftMotor.SetPercent(-1* direction * powerPercent);
     while((left_encoder.Counts() + right_encoder.Counts()) / 2. < distanceCounts);
  
     leftMotor.Stop();
     rightMotor.Stop();
 
 }
-void moveForward(float distanceInches)
+void moveForward(float distanceInches, int powerPercent = 40)
 {
     int direction = -1;
     right_encoder.ResetCounts();
     left_encoder.ResetCounts();
 
     int distanceCounts = (distanceInches * 318)/7.85; // Convert inches to encoder counts
-    rightMotor.SetPercent(direction * 40);
-    leftMotor.SetPercent(-1* direction * 40);
+    rightMotor.SetPercent(direction * powerPercent);
+    leftMotor.SetPercent(-1* direction * powerPercent);
     while((left_encoder.Counts() + right_encoder.Counts()) / 2. < distanceCounts);
  
     leftMotor.Stop();
@@ -190,6 +190,57 @@ void turnRight45(int percent)
     leftMotor.Stop();
 
 }
+void turnRight(int angle, int percent = 40)
+{
+    //Reset encoder counts
+
+    right_encoder.ResetCounts();
+
+    left_encoder.ResetCounts();
+
+    int counts = (angle/90.0)*(6.55*318)/7.85;
+
+
+    //Set both motors to desired percent
+
+    //hint: set right motor backwards, left motor forwards
+    rightMotor.SetPercent(percent);
+    leftMotor.SetPercent(percent);
+    //While the average of the left and right encoder is less than counts,
+    //keep running motors
+    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);  
+
+    //Turn off motors
+    rightMotor.Stop();
+    leftMotor.Stop();
+
+}
+void turnLeft(int angle, int percent = 40)
+{
+    //Reset encoder counts
+
+    right_encoder.ResetCounts();
+
+    left_encoder.ResetCounts();
+
+    int counts = (angle/90.0)*(6.55*318)/7.85;
+
+
+    //Set both motors to desired percent
+
+    //hint: set right motor backwards, left motor forwards
+    rightMotor.SetPercent(-1 * percent);
+    leftMotor.SetPercent(-1 * percent);
+    //While the average of the left and right encoder is less than counts,
+    //keep running motors
+    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);  
+
+    //Turn off motors
+    rightMotor.Stop();
+    leftMotor.Stop();
+
+}
+
 void followLine(int drivePercent)
 {
 
@@ -223,19 +274,14 @@ void followLine(int drivePercent)
 
         
 
-        if(leftOnBlack && rightOnBlack)
+        if((leftOnBlack && rightOnBlack) || (middleOnBlack && rightOnWhite) || (middleOnBlack && leftOnWhite))
         {
             leftMotor.Stop();
             rightMotor.Stop();
             break;
         }
 
-        else if((leftOnWhite && rightOnWhite) || (middleOnBlack))
-        {
-            leftMotor.SetPercent(forwardLeft);
-            rightMotor.SetPercent(forwardRight);
-        }
-        else if(leftOnWhite && rightOnBlack)
+        else if(leftOnWhite)
         {
             leftMotor.SetPercent(forwardLeft);
             rightMotor.SetPercent(forwardRight);
@@ -244,11 +290,6 @@ void followLine(int drivePercent)
         {
             leftMotor.SetPercent(forwardLeft);
             rightMotor.SetPercent(slowRight);
-        }
-        else if(rightOnGray || leftOnBlack)
-        {
-            leftMotor.SetPercent(slowLeft);
-            rightMotor.SetPercent(forwardRight);
         }
     }
 
@@ -286,56 +327,62 @@ void checkLight(){
            break;
        }
    }
+   if (isRed) {
+       LCD.WriteLine("Red Light Detected!");
+   }
+   else if (isBlue) {
+       LCD.WriteLine("Blue Light Detected!");
+   }
 }
-void ERCMain()
-{
-    /** 
-     * Pending change final instructions
-     * 
-    waitForTouchStart("Tap screen to start Milestone 2!"); // tap screen
+
+void oldERCMain(){
+    //waitForTouchStart("Tap screen to start Milestone 2!"); // tap screen
     waitForStartLight(); //checks if cds cell (red filter) detects red light
-    moveForward(2.5); // clicks button
-    Sleep(1000);
-    turnLeft90(20); // turn left to exterior wall
-    Sleep(1000);
-    turnLeft45(20); // turn left to ramp
-    Sleep(1000);
-    moveForward(24); // drive FULLY up ramp, no parts hanging behind
-    Sleep(1000);
-    lineFollow() // til ending when all black
-    turnLeft90(20); // turn left to face humidifier
-    Sleep(1000);
-    moveForward(4); //lineup to humidifier light
-    checkLight(); // check cds cell to see if red or blue light, click corresponding button
-
-    If Blue:{
-        find way back to ramp from blue position}
-    else if Red{
-        find way back to ramp from red position}
-
-    Move back down ramp, no parts hanging behind
-    
-    //finish rest after this stuff works...
-
-    */
-   
-    waitForTouchStart("Tap screen to start Milestone 2!"); // tap screen
-    waitForStartLight(); //checks if cds cell (red filter) detects red light
-    moveBackward(2.5); // clicks button
+    moveBackward(3, 50); // clicks button
     Sleep(250);
+    
+
     moveForward(2.5);
     turnRight45(20);
     moveForward(1);
     turnRight45(20);
-    moveForward(1.5);
-    turnLeft45(20);
-    moveForward(24);
-    moveForward(3);
-    turnLeft90(20);
+    moveForward(4);
+    turnLeft(50, 20);
+    moveForward(36, 50);
+
+    turnLeft(30, 20);
+    moveForward(1);
+    turnLeft(30, 20);
+    moveForward(1);
+    turnLeft(30, 20);
+    moveBackward(20);
+
+    Sleep(1000);
     followLine(20);
     moveForward(4);
     checkLight();
+}
+void ERCMain()
+{
+    
+    
+    waitForTouchStart("Tap screen to start Milestone 2!"); // tap screen
+    waitForStartLight(); //checks if cds cell (red filter) detects red light
+    moveBackward(3, 50); // clicks button
+    Sleep(250);
+    
+    waitForTouchStart("Tap screen to continue Milestone 2!"); // tap screen
+    moveForward(25, 50);
 
+    waitForTouchStart("Tap screen to continue Milestone 2!"); // tap screen
+    checkLight();
+    
+
+    
+    
+    
+
+    
     
 
 
